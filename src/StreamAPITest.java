765bf4363c34a2adc29221dd24f0259cd39a82c9
Appu Kumar <appu6688kumar@gmail.com>
2026-03-09 22:39:07 +0530

    stream api

---
+++ src/StreamAPITest.java
@@ -0,0 +1,11 @@
+public class StreamAPITest {
+     public static void main(String[] args){
+
+     }
+}
+
+/*
+
+
+
+ */
--- src/collection/StreamApi.java
+++ src/collection/StreamApi.java
@@ -3,6 +3,7 @@ package collection;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 class Stud {
     String name;
@@ -20,6 +21,12 @@ public class StreamApi {
                 .filter((e)->e.age>18).
                 forEach((e)-> System.out.println(e.age));
 
+        List<Integer> numbers = Arrays.asList(1,2,3,3,3,5,5,6,6,7,7);
+        List<Integer> output = numbers.stream()
+                .filter(n -> n % 2 == 0)
+                .map(n -> n * 2)
+                .collect(Collectors.toList());
+
     }
 }
 /*
@@ -32,6 +39,5 @@ public class StreamApi {
  Before using stream api, convert collection into stream and then use intermediate and terminal methods like filter, map, and reduce
 
  Google--> terminal and intermediate methods okay
-
 
  */
